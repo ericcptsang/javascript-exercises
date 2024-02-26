18123482e4398decfd59c6084a95de5d00b2c1f3
Eric C P Tsang <113884449+ericcptsang@user.noreply.github.com.>
2024-02-26 23:52:33 +0800

    submitted

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    arr.forEach(element => {
+        if(!args.includes(element)){
+            result.push(element);
+        }
+    });
+    return result;
 };
 
 // Do not edit below this line

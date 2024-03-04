469e187bb740caa3644e69820ed1cf96f35afa61
Eric C P Tsang <113884449+ericcptsang@user.noreply.github.com.>
2024-03-04 18:25:01 +0800

    submitted ans

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if(!Number.isInteger(int1) ||
+       !Number.isInteger(int2) ||
+       int1 < 0 || int2 < 0){
+        return "ERROR";
+    }
+    let small, big;
+    if(int1 < int2){
+        small = int1;
+        big = int2;
+    } else {
+        small = int2;
+        big = int1;
+    }
+    let sum = 0;
+    for(let i = small; i <= big; i++){
+        sum+=i; 
+    }
+    return sum;
 };
 
 // Do not edit below this line
